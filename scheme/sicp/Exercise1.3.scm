6a1196ff5ad84e4bee5a46a30cd769dfaa6e5677
gkhays <gkhays@gmail.com>
2016-03-13 18:14:38 -0500

    Exercise 1.3
    Thought I could do it with if but ended up using cond.

---
+++ scheme/sicp/Exercise1.3.scm
@@ -0,0 +1,8 @@
+(define (square x)
+    (* x x))
+(define (sum-of-squares x y)
+    (+ (square x) (square y)))
+(define (sum-and-square x y z)
+    (cond ((and (> x y) (> y z)) (sum-of-squares x y))
+          ((and (< x y) (< y z)) (sum-of-squares y z))
+          (else (sum-of-squares x z))))
